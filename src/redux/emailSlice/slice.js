6b66bb13fe81b53b0be53e416d448f29f5cd4b2e
Anton.O <boogeymanqq@gmail.com>
2024-08-19 13:44:54 +0300

    create obj for send email

---
+++ src/redux/emailSlice/slice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+	success: false,
+	loading: false,
+	error: null,
+	emailObj: {},
+	successMessage: ''
+}
+
+export const emailSlice = createSlice({
+	name: 'emailSlice',
+	initialState,
+	reducers: {
+		sendEmailRequest: state => {
+			state.loading = true
+			state.error = null
+			state.success = false
+		},
+		sendEmailSuccess: (state, action) => {
+			state.loading = false
+			state.success = true
+		},
+		sendEmailFailure: (state, action) => {
+			state.loading = false
+			state.error = action.payload
+		},
+		setEmailObj: (state, action) => {
+			state.emailObj = action.payload
+		},
+		setSuccessMessage: (state, action) => {
+			state.successMessage = action.payload
+		}
+	},
+})
+
+export const { sendEmailRequest, sendEmailSuccess, sendEmailFailure, setEmailObj, setSuccessMessage } =
+emailSlice.actions
+export default emailSlice.reducer
