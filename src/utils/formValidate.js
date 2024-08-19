b0b7e3775ac00990d4f9f9aa149a4ee3f872cd13
Anton.O <boogeymanqq@gmail.com>
2024-08-19 13:43:53 +0300

    add validate utils

--- src/utils/formValidate.js
+++ src/utils/formValidate.js
@@ -1,12 +1,28 @@
 
-export const validate = values => {
-  const errors = {};
+export const required = (value, values) => {
+  if (value) return undefined
 
-  if (!values.Email) {
-    errors.Email = 'Email is required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.Email)) {
-    errors.Email = 'Invalid email address';
-  }
-
-  return errors;
+  return 'Field is required!'
 };
+
+// Проверка на валидный email
+export const validateEmail = (value) => {
+  if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+    return 'Invalid email address!';
+  }
+  return undefined;
+};
+
+
+
+export const maxLengthCreator = (maxLength) => (value) => {
+  if (value && value.length > maxLength) return `Max length ${{maxLength}} symbols!`
+
+  return undefined
+};
+
+export const minLength = (value) => {
+  if (value && value.length < 2) return 'Min length 2 symbols!'
+
+  return undefined
+}
