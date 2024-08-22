a07663ae09cbd76f0760956c86add4262b51fe47
Anton Olshevskiy <95619031+Boogeymanqq@users.noreply.github.com>
2024-08-22 11:16:28 +0300

    add styled-components, add prettierrc, create mui component for typography, create file styles for modal component (#4)
    need npm i

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -1,18 +1,28 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { TextField } from '@mui/material';
+import React from 'react';
 
-export const InputField = ({input, multiline, rows,isRequired = false, isFullWidth = false, helperText, id, label, meta: { touched, error}}) => {
+export const InputField = ({
+  input,
+  multiline,
+  rows,
+  isRequired = false,
+  isFullWidth = false,
+  helperText,
+  id,
+  label,
+  meta: { touched, error },
+}) => {
   return (
-						<TextField
-            {...input}
-            error={touched && error && isRequired}
-						    required={isRequired}
-						    fullWidth={isFullWidth}
-			          id={id}
-			          label={label}
-                helperText={touched && error && (error || helperText)}
-                rows={rows}
-                multiline={multiline}
-			        />
-  )
-}
+    <TextField
+      {...input}
+      error={touched && error && isRequired}
+      required={isRequired}
+      fullWidth={isFullWidth}
+      id={id}
+      label={label}
+      helperText={touched && error && (error || helperText)}
+      rows={rows}
+      multiline={multiline}
+    />
+  );
+};
--- src/redux/sagas/emailSaga.js
+++ src/redux/sagas/emailSaga.js
@@ -1,42 +1,46 @@
-import { call, put, takeLatest } from 'redux-saga/effects'
+import { call, put, takeLatest } from 'redux-saga/effects';
 import {
-	sendEmailRequest,
-	sendEmailSuccess,
-	sendEmailFailure,
-	setSuccessMessage,
-} from '../emailSlice/slice'
-import emailjs from '@emailjs/browser'
+  sendEmailRequest,
+  sendEmailSuccess,
+  sendEmailFailure,
+  setSuccessMessage,
+} from '../emailSlice/slice';
+import emailjs from '@emailjs/browser';
 
 // Функция для отправки email с использованием emailjs
 function sendEmail(data) {
-
-	emailjs
-      .send(import.meta.env.VITE_APP_SERVICE_ID, import.meta.env.VITE_APP_TEMPLATE_ID, data, {
+  emailjs
+    .send(
+      import.meta.env.VITE_APP_SERVICE_ID,
+      import.meta.env.VITE_APP_TEMPLATE_ID,
+      data,
+      {
         publicKey: import.meta.env.VITE_APP_PUBLIC_KEY,
-      })
-      .then(
-        () => {
-          console.log('Success!');
-        },
-        (error) => {
-          console.log('Failed...', error);
-        },
-      );
+      },
+    )
+    .then(
+      () => {
+        console.log('Success!');
+      },
+      (error) => {
+        console.log('Failed...', error);
+      },
+    );
 }
 
 function* sendEmailSaga(action) {
-	try {
-		const response = yield call(sendEmail, action.payload)
-		yield put(sendEmailSuccess(response))
+  try {
+    const response = yield call(sendEmail, action.payload);
+    yield put(sendEmailSuccess(response));
 
-		yield put(setSuccessMessage('Your message is send!'))
-	} catch (error) {
-		yield put(sendEmailFailure(error.toString()))
-	}
+    yield put(setSuccessMessage('Your message is send!'));
+  } catch (error) {
+    yield put(sendEmailFailure(error.toString()));
+  }
 }
 
 function* watchSendEmailSaga() {
-	yield takeLatest(sendEmailRequest.type, sendEmailSaga)
+  yield takeLatest(sendEmailRequest.type, sendEmailSaga);
 }
 
-export default watchSendEmailSaga
+export default watchSendEmailSaga;
